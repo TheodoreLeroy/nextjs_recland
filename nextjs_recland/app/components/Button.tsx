04da38e4c7aeb9310867d3aba240a406f4bd872f
Theodore Leroy <hunganhyb0503@gmail.com>
2026-05-26 05:34:15 +0700

    feat: implement navigation responsive for webpage

--- nextjs_recland/app/components/Button.tsx
+++ nextjs_recland/app/components/Button.tsx
@@ -1,12 +1,64 @@
-interface ButtonProps{
-    children: React.ReactNode
-    variants: "primary" | "secondary" | "outlined" | "danger"
-    classes: string
-}
-export const Button = ({children, variants, classes} : ButtonProps) => {
-    return(
-        <div className="">
+type ButtonVariant = "primary" | "secondary" | "outlined";
 
-        </div>
-    )
+interface ButtonProps {
+    children: React.ReactNode;
+    variant?: ButtonVariant;
+    className?: string;
+    onClick?: () => void;
+    type?: "button" | "submit" | "reset";
+}
+export default function Button({
+    children,
+    variant = "primary",
+    className = "",
+    onClick,
+    type = "button",
+}: ButtonProps) {
+    const baseStyles = `
+        w-full
+        h-[50px]
+        rounded-[18px]
+        text-3xl
+        font-semibold
+        transition-all
+        duration-300
+        hover:scale-[1.02]
+        active:scale-[0.98]
+        text-nowrap
+    `;
+
+    const variants = {
+        primary: `
+            bg-[#F2A646]
+            text-[#006C8C]
+            hover:opacity-90
+        `,
+
+        secondary: `
+            bg-[#006C8C]
+            text-white
+            hover:opacity-90
+        `,
+
+        outlined: `
+            border-2
+            border-[#F2A646]
+            bg-white
+            text-[#F2A646]
+            hover:bg-[#FFF7ED]
+        `,
+    };
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={`
+                ${baseStyles}
+                ${variants[variant]}
+                ${className}
+            `}
+        >
+            {children}
+        </button>
+    );
 }
--- nextjs_recland/app/components/TopNavBar.tsx
+++ nextjs_recland/app/components/TopNavBar.tsx
@@ -1,39 +1,59 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { useEffect, useState, useRef } from "react";
 import { IoIosMenu } from "react-icons/io";
+import Button from "./Button";
+import logo from "./logo.png";
 export const TopNavBar = () => {
     const [isScroll, setIsScroll] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
     const menuRef = useRef<HTMLDivElement>(null);
+    const menuItems: { [key: string]: string } = {
+        "Tìm việc": "/jobs",
+        "Công ty": "/company",
+        Blog: "/blog",
+        "Giới thiệu": "/about",
+        "Liên hệ": "/contact",
+    };
 
-    useEffect(() => {
-        function handleClickOutside(event: MouseEvent) {
-            if (
-                menuRef.current &&
-                !menuRef.current.contains(event.target as Node)
-            ) {
-                setIsMenuOpen(false);
-            }
-        }
+    // useEffect(() => {
+    //     function handleClickOutside(event: MouseEvent) {
+    //         if (
+    //             menuRef.current &&
+    //             !menuRef.current.contains(event.target as Node)
+    //         ) {
+    //             setIsMenuOpen(false);
+    //         }
+    //     }
 
-        document.addEventListener("mousedown", handleClickOutside);
+    //     document.addEventListener("mousedown", handleClickOutside);
 
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
+    //     return () => {
+    //         document.removeEventListener("mousedown", handleClickOutside);
+    //     };
+    // }, []);
 
     useEffect(() => {
         const handleScroll = () => {
             setIsScroll(window.scrollY > 0);
         };
 
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target as Node)
+            ) {
+                setIsMenuOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
         window.addEventListener("scroll", handleScroll);
 
         return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
@@ -43,73 +63,113 @@ export const TopNavBar = () => {
     };
 
     return (
-        <>
-            <nav id="navbar" className="w-screen h-30 flex justify-between">
-                {/* logo */}
-                <div className="flex items-center justify-center px-10">
-                    <Link href="#">
-                        <img
-                            src="images\graphics\logo-white752b.png"
-                            alt="Recland white logo"
-                            className="
-                        w-40 self-center"
+        <div className={`fixed top-0 w-full flex justify-center transition-all duration-300 ${isScroll ? "bg-topbar-color": ""}`}>
+            <nav
+                id="navbar"
+                className="w-full h-30 flex items-center justify-between tablet:w-fit"
+            >
+                {/* Logo */}
+                <div className="flex items-center px-10">
+                    <Link href="/home">
+                        <Image
+                            src="/images/graphics/logo-white752b.png"
+                            alt="Recland logo"
+                            width={120}
+                            height={120}
+                            priority
                         />
                     </Link>
                 </div>
-                {/* menu mobile */}
-                <div className="flex justify-center items-center px-10">
-                    <button type="button" onClick={openMenu}>
-                        <IoIosMenu fill="white" size={32} />
-                    </button>
-                    <div
-                        ref={menuRef}
-                        className={`
-                                absolute
-                                flex
-                                flex-col
-                                w-screen
-                                h-fit
-                                left-0
-                                top-0
-                                transition-all                     
-                                duration-300   
-                                bg-white                    
-                                ${
-                                    isMenuOpen
-                                        ? "translate-x-0"
-                                        : "translate-x-full"
-                                }`}
-                    >
-                        <div className="w-screen h-30 border-b border-gray-200 flex items-center">
-                            <img
-                                src="images\graphics\logo.png"
-                                alt="Recland logo"
-                                className="h-16 px-6"
-                            />
-                        </div>
-                        <ul
-                            id="mobile_nav"
-                            className="flex flex-col text-3xl px-6 leading-10 py-10 font-medium text-gray-400"
-                        >
-                            <li className="">
-                                <Link href="/jobs">Tìm việc</Link>
+
+                {/* Desktop Menu */}
+                <div className="hidden tablet:flex items-center gap-10 px-10">
+                    <ul className="flex items-center gap-8">
+                        {Object.entries(menuItems).map(([key, value]) => (
+                            <li key={key} className="text-nowrap">
+                                <Link
+                                    href={value}
+                                    className="text-gray-300 text-2xl hover:text-gray-300 transition"
+                                >
+                                    {key}
+                                </Link>
                             </li>
-                            <li>
-                                <Link href="/company">Công ty</Link>
-                            </li>
-                            <li>
-                                <Link href="/blog">Blog</Link>
-                            </li>
-                            <li>
-                                <Link href="/about">Giới thiệu</Link>
-                            </li>
-                            <li>
-                                <Link href="/contact">Liên hệ</Link>
-                            </li>
-                        </ul>
+                        ))}
+                    </ul>
+
+                    <div className="flex items-center gap-4">
+                        <Link href="/home">
+                            <Image
+                                src="/images/flag/flag-en752b.svg"
+                                alt="America flag image"
+                                width={60}
+                                height={20}
+                            ></Image>
+                        </Link>
+                        <Button>Cộng tác viên</Button>
+
+                        <Button variant="outlined">Nhà tuyển dụng</Button>
                     </div>
                 </div>
+
+                {/* Mobile Menu Button */}
+                <div className="tablet:hidden flex items-center px-10">
+                    <button type="button" onClick={openMenu}>
+                        <IoIosMenu fill="white" size={36} />
+                    </button>
+                </div>
+
+                {/* Mobile Menu */}
+                <div
+                    ref={menuRef}
+                    className={`
+                    tablet:hidden
+                    fixed top-0 right-0
+                    w-screen h-fit
+                    bg-white
+                    py-10
+                    z-50
+                    transition-transform duration-500 ease-in-out
+                    ${isMenuOpen ? "translate-x-0" : "translate-x-full"}
+                `}
+                >
+                    <ul>
+                        <li className="px-5 pb-10 border-b border-gray-300 mb-2">
+                            <Link href="/home">
+                                <Image
+                                    src="/images/graphics/logo.png"
+                                    alt="Recland logo"
+                                    width={145}
+                                    height={145}
+                                    priority
+                                />
+                            </Link>
+                        </li>
+
+                        {Object.entries(menuItems).map(([key, value]) => (
+                            <li key={key}>
+                                <Link
+                                    href={value}
+                                    className="
+                                    block
+                                    text-3xl
+                                    hover:text-gray-300
+                                    px-5
+                                    py-5
+                                "
+                                >
+                                    {key}
+                                </Link>
+                            </li>
+                        ))}
+
+                        <div className="flex flex-col gap-5 px-5 mt-5">
+                            <Button>Cộng tác viên</Button>
+
+                            <Button variant="outlined">Nhà tuyển dụng</Button>
+                        </div>
+                    </ul>
+                </div>
             </nav>
-        </>
+        </div>
     );
 };
